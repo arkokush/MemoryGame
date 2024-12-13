13a084b80876b3c7001e6672b5df67e647e8619a
akokush <kokush.arkady@gmail.com>
2024-12-13 13:46:21 -0500

    init comit

--- MemoryGame/ContentView.swift
+++ MemoryGame/ContentView.swift
@@ -9,16 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
         .padding()
+        .foregroundColor(.orange)
+        
     }
 }
-
+struct CardView: View{
+    var isFaceUp:Bool = false
+    var body: some View{
+        ZStack (content: {
+            if(isFaceUp){
+                RoundedRectangle(cornerRadius: 30)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 30)
+                    .strokeBorder(lineWidth: 4)
+                Text("😎").font(.largeTitle)}
+            else{
+                RoundedRectangle.init(cornerRadius: 30)
+            }
+            
+        })
+    }
+}
 #Preview {
     ContentView()
 }
