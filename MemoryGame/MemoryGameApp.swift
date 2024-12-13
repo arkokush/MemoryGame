1685d62a1c6946c34b2eb09b24f9a55107684139
akokush <kokush.arkady@gmail.com>
2024-12-13 12:01:41 -0500

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Arkady Kokush on 12/13/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MemoryGameTests/MemoryGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameTests.swift
+//  MemoryGameTests
+//
+//  Created by Arkady Kokush on 12/13/24.
+//
+
+import Testing
+@testable import MemoryGame
+
+struct MemoryGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
