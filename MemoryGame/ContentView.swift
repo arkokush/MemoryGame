973940837f8205671f518fc53d76e15bfae77979
akokush <kokush.arkady@gmail.com>
2024-12-20 09:20:00 -0500

    UI changes

--- MemoryGame/ContentView.swift
+++ MemoryGame/ContentView.swift
@@ -8,36 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis: [String] = ["😎", "🤓","🧐","🥸","🥸"]
-
+    @State var emojis: [String] = ["😎", "🤓","🧐","🥸","😳","🧐","🤓","🥸","😳","😎"]
+    @State var color: Color = .red
+     var cardCount = 10
     var body: some View {
-        HStack {
-            ForEach(emojis.indices,id: \.self){index in
-                CardView(content:emojis[index])
-
+        VStack{
+            Text("Memorize!")
+                .fontDesign(.serif)
+                .font(.title2)
+            ScrollView{
+                cards
             }
-          
+            Spacer()
+            emojiChangers
         }
         .padding()
-        .foregroundColor(.orange)
+    }
+    
+    func emojiChanger(theme:[String], colorSelected:Color, symbol: String) -> some View{
+        Button(action: {
+            emojis = theme
+            color = colorSelected
+            emojis.shuffle()
+        }, label: {
+            Text(symbol)
+                .font(.largeTitle)
+        })
+    }
+    
+    var faces: some View{
+        emojiChanger(theme: ["😎", "🤓","🧐","🥸","😳","🧐","🤓","🥸","😳","😎"] , colorSelected: .red,symbol:"😎" )
+        
+                             }
+    var halloween: some View{
+        emojiChanger(theme: ["🎃", "👻","🧙🏻‍♀️","🕷️","💀","🧙🏻‍♀️","👻","🕷️","💀","🎃"] ,colorSelected: .orange, symbol:"🎃" )
+        
+                             }
+    var christmas: some View{
+        emojiChanger(theme: ["🤶🏻", "🎄","🎁","🎉","🎅🏻","🎁","🎄","🎉","🎅🏻","🤶🏻"] ,colorSelected: .green, symbol:"🎄" )
+        
+                             }
+    
+   var emojiChangers: some View{
+        HStack{
+            faces
+            Spacer()
+            halloween
+            Spacer()
+            christmas
+        }
         
     }
+    
+    var cards: some View{
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 70))] ) {
+            ForEach(0..<cardCount,id: \.self){index in
+                CardView(content:emojis[index])
+                    .aspectRatio(2/3,contentMode: .fit)
+            }
+            
+        }
+        .foregroundColor(color)
+    }
+   
 }
 struct CardView: View{
     let content: String
-    @State var isFaceUp = false
+    @State var isFaceUp = true
     var body: some View{
         ZStack {
             let base = RoundedRectangle(cornerRadius: 20)
-            if(isFaceUp){
+            Group{
                 base
                     .foregroundColor(.white)
                 base
                     .strokeBorder(lineWidth: 4)
-                Text(content).font(.largeTitle)}
-            else{
-                base
-            }
+                Text(content).font(.largeTitle)
+            }.opacity(isFaceUp ? 1:0)
+           
+            base.opacity(isFaceUp ? 0:1)
             
         }
         .onTapGesture {
